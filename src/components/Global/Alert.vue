f57e993c9b9dd17a71ed0d927060d296bb64afe5
David Peralta <peraltadavidr@outlook.com>
2020-07-06 05:49:02 +0800

    Add title on alert

--- src/components/Global/Alert.vue
+++ src/components/Global/Alert.vue
@@ -1,5 +1,6 @@
 <template>
   <div :class="`lost-alert ${type}`">
+    <p class="title" v-if="title">{{title}}</p>
     <span>
       <slot></slot>
     </span>
@@ -12,7 +13,12 @@ export default {
     type: {
       type: String,
       default: 'primary',
+      required: false,
       validator: (value) => ['primary', 'success', 'error', 'info'].indexOf(value) !== -1,
+    },
+    title: {
+      type: String,
+      required: false,
     },
   },
 };
@@ -21,9 +27,16 @@ export default {
 <style lang="scss" scoped>
 div.lost-alert{
   margin: 8px 0;
-  padding: 4px 10px;
+  padding: 8px 10px;
   border-radius: size(componentBorderRadius);
-  color: rgb(243, 243, 243);
+  color: color(text);
+
+  .title {
+    font-weight: 600;
+    font-size: 10px;
+    line-height: 1;
+    margin-top: 4px;
+  }
 
   span {
     font-size: 12px;
@@ -33,6 +46,12 @@ div.lost-alert{
     color: color(text) !important;
     background: rgba(color(primary), 0.9);
     border: solid 2px darken(color(primary), 5%)
+  }
+
+  &.warning {
+    color: color(text) !important;
+    background: rgba(color(warning), 0.9);
+    border: solid 2px darken(color(warning), 5%)
   }
 
   &.error {
